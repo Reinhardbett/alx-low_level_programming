216d81b84ac43cbffb68307e488231557f8cd457
Reinhardbett <bettkiptoo46@gmail.com>
2022-09-15 10:13:51 -0700

    Rearrange nested if

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -12,12 +12,16 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90 || (c >= 97 && c <= 122))
-			{
-			return (1);
-			}
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+		if (c >= 97 && c <= 122)
+		{	
+		return (1);
+		}
 			else
 			{
 			return (0);
-			}
-			}
+			}				
+}
