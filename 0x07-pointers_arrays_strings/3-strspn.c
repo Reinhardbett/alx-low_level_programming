3a53d3df58bf4afa0401640217de33e3fc4f86dc
Reinhardbett <bettkiptoo46@gmail.com>
2022-09-27 09:44:32 -0700

    get length of prefix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - gets length of prefix string
+ *
+ * @s: string array
+ * @accept: location segment
+ *
+ * Return: number of bytes in initial segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int z = 0, x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] != 32)
+		{
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == accept[y])
+					z++;
+			}
+		}
+		else
+			return (z);
+	}
+	return (z);
+}
