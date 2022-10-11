d4db0410add17ecf3e5f185fa1b68b454d3652b9
Reinhardbett <bettkiptoo46@gmail.com>
2022-10-11 07:30:30 -0700

    Updated README.md

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - function
+ *
+ * @c: parameter
+ *
+ * Return: value
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *_puts - print to stdout
+  *
+  *return: (0)
+  */
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to bet he best hacker\"\n\t - Kevin Mitnick");
+	return (0);
+}
